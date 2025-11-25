bb02777de5d753e327bb97ac1af90075497c7d5d
Checkers677 <stephensekabira677@gmail.com>
2025-11-26 01:39:00 +0300

    Add custom Document component for Next.js

---
+++ _document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="cq4_sWVg30SF5uJ7MzrlXydLzDVZt8lEKRmaidLtKCE" />
+        {/* other meta tags */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+    }
